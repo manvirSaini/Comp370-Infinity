13c05582664307748ce97172f9b15e7e3f23a134
manvirSaini <59299230+manvirSaini@users.noreply.github.com>
2021-02-28 12:22:11 -0800

    Trying to test database. ISSUE: cannot get database to appear in 'Database Inspector' and make 'Toast' appear when field is left empty. So far only tested courses entity.
    Made Changes to:
    activity_main.xml
    DBTestActivity.java

--- app/src/main/java/com/example/infinity_courseproject/DBTestActivity.java
+++ app/src/main/java/com/example/infinity_courseproject/DBTestActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -22,18 +24,41 @@ public class DBTestActivity extends AppCompatActivity {
     private CourseViewModel courseViewModel;
     private RoutineViewModel routineViewModel;
     private PeriodViewModel periodViewModel;
-    private TextView textView1;
-    private TextView textView2;
-    private TextView textView3;
+    private TextView enterTitle;
+    private TextView enterProf;
+    private TextView enterDescription;
+    private Button saveInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //textView1 = findViewById(R.id.text1);
-        textView2 = findViewById(R.id.text2);
-        //textView3 = findViewById(R.id.text3);
+        enterTitle = findViewById(R.id.enter_title);
+        enterProf = findViewById(R.id.enter_prof);
+        enterDescription = findViewById(R.id.enter_description);
+        saveInfo = findViewById((R.id.save_button));
+
+
+        saveInfo.setOnClickListener(view -> {
+
+            if(!TextUtils.isEmpty(enterTitle.getText()) &&
+                    !TextUtils.isEmpty(enterProf.getText()) &&
+                    !TextUtils.isEmpty(enterDescription.getText())) {
+
+
+                Course course = new Course(
+                        enterTitle.getText().toString(),
+                        enterProf.getText().toString(),
+                        enterDescription.getText().toString());
+
+                CourseViewModel.insert(course);
+
+            }else{
+                Toast.makeText(this, R.string.empty, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
 
         //initialize viewmodels
         routineViewModel = new ViewModelProvider.AndroidViewModelFactory(
@@ -42,41 +67,42 @@ public class DBTestActivity extends AppCompatActivity {
                 this.getApplication()).create(CourseViewModel.class);
         periodViewModel = new ViewModelProvider.AndroidViewModelFactory(
                 this.getApplication()).create(PeriodViewModel.class);
+
         //delete all previous entries
-        RoutineViewModel.deleteAll();
-        CourseViewModel.deleteAll();
-        PeriodViewModel.deleteAll();
+//        RoutineViewModel.deleteAll();
+//        CourseViewModel.deleteAll();
+//        PeriodViewModel.deleteAll();
 
         //creating observers
         courseViewModel.getAllCourses().observe(this, new Observer<List<Course>>() {
             @Override
             public void onChanged(List<Course> courses) {
                 if (!courses.isEmpty()) {
-                    textView1.setText(courses.get(0).getTitle());
+                    enterTitle.setText(courses.get(0).getTitle());
                 }
 
             }
         });
+//
+//        routineViewModel.getAllRoutines().observe(this, new Observer<List<Routine>>() {
+//            @Override
+//            public void onChanged(List<Routine> routines) {
+//                if (!routines.isEmpty()) {
+//                    textView2.setText(routines.get(0).getTitle());
+//                }
+//            }
+//        });
+//
+//        periodViewModel.getAllPeriods().observe(this, new Observer<List<Period>>() {
+//            @Override
+//            public void onChanged(List<Period> periods) {
+//                if (!periods.isEmpty()) {
+//                    textView3.setText(String.valueOf(periods.get(0).getPosition()));
+//                }
+//            }
+//        });
 
-        routineViewModel.getAllRoutines().observe(this, new Observer<List<Routine>>() {
-            @Override
-            public void onChanged(List<Routine> routines) {
-                if (!routines.isEmpty()) {
-                    textView2.setText(routines.get(0).getTitle());
-                }
-            }
-        });
-
-        periodViewModel.getAllPeriods().observe(this, new Observer<List<Period>>() {
-            @Override
-            public void onChanged(List<Period> periods) {
-                if (!periods.isEmpty()) {
-                    textView3.setText(String.valueOf(periods.get(0).getPosition()));
-                }
-            }
-        });
-
-        //creating entries
+//        creating entries
 //        Routine routine = new Routine("routine", new boolean[7]);
 //        Course course = new Course("course");
 //
