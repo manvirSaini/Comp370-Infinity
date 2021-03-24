2356e5c308d2a79295342d008e91d1018d5d43dd
Aibo <aibo.huang@student.ufv.ca>
2021-03-23 20:44:06 -0700

    correct spelling

--- app/src/main/java/com/example/infinity_courseproject/CourseDialog.java
+++ app/src/main/java/com/example/infinity_courseproject/CourseDialog.java
@@ -40,7 +40,7 @@ public class CourseDialog extends BaseDialog {
         et_1 = window.findViewById(R.id.et_1);
         et_2 = window.findViewById(R.id.et_2);
         tv_commit = window.findViewById(R.id.tv_commit);
-        tv_cannal = window.findViewById(R.id.tv_cannal);
+        tv_cannal = window.findViewById(R.id.tv_cancel);
         tv_title = window.findViewById(R.id.tv_title);
         //set view
         if (bean == null) {
